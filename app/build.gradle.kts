f9b61b76ef525883ac88c438f7d38036bf35fb95
Shobhit Jain <bjshobhit8@gmail.com>
2023-09-13 23:56:04 +0530

    Chat Fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("com.google.firebase:firebase-database:20.2.2")
     implementation("com.google.firebase:firebase-storage:20.2.1")
     implementation("com.google.firebase:firebase-firestore:24.7.1")
+    implementation("com.firebaseui:firebase-ui-firestore:8.0.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -59,4 +60,6 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.github.fornewid:neumorphism:0.3.2")
     implementation("com.hbb20:ccp:2.7.3")
+    implementation("com.squareup.picasso:picasso:2.71828")
+
 }
